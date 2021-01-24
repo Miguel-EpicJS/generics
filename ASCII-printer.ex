b90785c6a13e1d31b46574cbe607f148affac7f3
CodigoDeSenior <migueljnm840@gmail.com>
2021-01-24 14:44:11 -0300

    letras f, g, h, i, j e k

--- ASCII-printer.ex
+++ ASCII-printer.ex
@@ -12,6 +12,18 @@ for letra <- String.graphemes(letras) do
       IO.puts("  _____  \n |  __ \\ \n | |  | |\n | |  | |\n | |__| |\n |_____/ \n")
     letra == "e" or letra == "E" ->
       IO.puts("  ______ \n |  ____|\n | |__   \n |  __|  \n | |____ \n |______|\n")
+    letra == "f" or letra == "F" ->
+      IO.puts("  ______ \n |  ____|\n | |__   \n |  __|  \n | |     \n |_|     \n")
+    letra == "g" or letra == "G" ->
+      IO.puts("   _____ \n  / ____|\n | |  __ \n | | |_ |\n | |__| |\n  \\_____|\n")
+    letra == "h" or letra == "H" ->
+      IO.puts("  _    _ \n | |  | |\n | |__| |\n |  __  |\n | |  | |\n |_|  |_|\n")
+    letra == "i" or letra == "I" ->
+      IO.puts("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n")
+    letra == "j" or letra == "J" ->
+      IO.puts("       _ \n      | |\n      | |\n  _   | |\n | |__| |\n \\______/\n")
+    letra == "k" or letra == "K" ->
+      IO.puts("  _  __\n | |/ /\n | ' / \n |  <  \n | . \\ \n |_|\\_\\\n")
     true -> IO.puts "."
 
   end
